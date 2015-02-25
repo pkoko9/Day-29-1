4ef8ae8b12efbf7804ad456d3ba06ec41e2e3756
Paul Kokoszyna <pkoko9@hotmail.com>
2015-02-24 22:28:13 -0600

    Font awesome link issue preventing carrot icons from loading - all other functionality working

--- homework/scripts/controllers.js
+++ homework/scripts/controllers.js
@@ -3,18 +3,38 @@ angular.module('basic.controllers', [])
 
 	$scope.states=[];
 	$scope.reverseStates=[];
+	$scope.filterBy='';
+	$scope.stateArrow = true;
 
 	$http.get('http://tiny-pizza-server.herokuapp.com/collections/fancy-table').success(function(response){
 		$scope.states=response;
-		console.log($scope.states);
+		// console.log($scope.states);
+
+		$scope.states = _.sortBy($scope.states, function(element) {
+			return element.name + element.abbreviation;
+		})
+
+		$scope.secondArray = _.sortBy($scope.states, function(element) {
+			return element.name + element.abbreviation;
+			console.log($scope.secondArray);
+		})
 
 	});
 
 	$scope.onNameClick=function() {
-		$scope.reverseStates=$scope.states;
-		$scope.states=$scope.reverseStates.reverse();
+		$scope.reverseStates=$scope.secondArray;
+		$scope.secondArray=$scope.reverseStates.reverse();
+		console.log($scope.secondArray);
+	};
 
-	}
+	$scope.$watch('filterBy', function(){
+	 $scope.secondArray=_.filter($scope.states, function(element) {
+		return element.name.toLowerCase().indexOf($scope.filterBy.toLowerCase()) === 0 ||
+		element.abbreviation.toLowerCase().indexOf($scope.filterBy.toLowerCase()) === 0;
 
+	});
 
+	});
+
+	$scope.stateArrow = !$scope.stateArrow;
 });
